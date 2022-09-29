234dfc71ad8be943915e183b3fca392f1c126c25
Max <chenmax02@gmail.com>
2022-09-29 13:50:13 +0200

    Update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,43 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Participant;
-use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
     {
-        $user = new Participant();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_home');
-        }
-
         return $this->render('main/index.html.twig', [
-            'registrationForm' => $form->createView(),
+            'controller_name' => 'MainController',
         ]);
     }
 }
