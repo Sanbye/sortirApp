47c5dbe5afe338b4cd7d2a917f0d9e6ffd6f09ab
Max <chenmax02@gmail.com>
2022-09-29 14:07:51 +0200

    Update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,8 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $Participant = new Participant();
+        $ParticipantForm = $this->createForm(ParticipantType::class, $Participant);
+        return $this->render('main/index.html.twig', ["ParticipantForm" => $ParticipantForm->createView()]);
     }
 }
