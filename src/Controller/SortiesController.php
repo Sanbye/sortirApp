2f3dbad580b9ccf0ee44cf93145abe02210c941c
Max <chenmax02@gmail.com>
2022-09-29 14:33:00 +0200

    Update

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -9,9 +9,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class SortiesController extends AbstractController
 {
     #[Route('/sorties', name: 'app_sorties')]
-    public function index(): Response
+    public function afficher(): Response
     {
-        return $this->render('sorties/index.html.twig', [
+        return $this->render('sorties/', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+
+    #[Route('/sorties/creer', name: 'app_sorties_creer')]
+    public function creer(): Response
+    {
+        return $this->render('sorties/', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+
+    #[Route('/sorties/modifier', name: 'app_sorties_modifier')]
+    public function modifier(): Response
+    {
+        return $this->render('sorties/', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+
+    #[Route('/sorties/annuler', name: 'app_sorties_annuler')]
+    public function annuler(): Response
+    {
+        return $this->render('sorties/', [
             'controller_name' => 'SortiesController',
         ]);
     }
