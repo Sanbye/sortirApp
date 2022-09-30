258d2735d659fdc3cb595c7b6d7a9224e6f6b75d
Max <chenmax02@gmail.com>
2022-09-30 13:55:04 +0200

    Affichage de la page de connexion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function create(Request $request): Response
     {
         $Participant = new Participant();
         $ParticipantForm = $this->createForm(LoginType::class, $Participant);
+        $ParticipantForm->handleRequest($request);
+
+        if ($ParticipantForm->isSubmitted() && $ParticipantForm->isValid()) {
+            $this->redirectToRoute('/home');
+        }
+
         return $this->render('main/index.html.twig', ["ParticipantForm" => $ParticipantForm->createView()]);
     }
 }
