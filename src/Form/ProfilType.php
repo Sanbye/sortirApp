9972aa22f7c27045499f362e3981cd10606df7db
Max <chenmax02@gmail.com>
2022-10-02 01:58:39 +0200

    Création et mise en place de ProfilType.php

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('telephone', NumberType::class)
+            ->add('email', EmailType::class)
+            ->add('motPasse', PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
