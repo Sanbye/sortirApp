da3cdce12e3fa170746bde41fdf8bcff5501fd9d
Max <chenmax02@gmail.com>
2022-10-01 15:49:51 +0200

    Route fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,15 +23,12 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('main/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route('logout', name: 'logout',methods: ['GET'])]
     public function logout(): void
     {
-
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-
-
     }
 }
