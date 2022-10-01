5c5c9a1d4c3954bdaa61fb3db73437be108a0e45
Max <chenmax02@gmail.com>
2022-10-02 01:54:59 +0200

    Séparation de Profil en Profil_Afficher et Profil_Modifier

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ProfilType;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
+use http\Client\Curl\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,20 +41,27 @@ class ParticipantController extends AbstractController
     #[Route('profil', name: 'profil')]
     public function afficherProfil(ParticipantRepository $participantRepository): Response
     {
+
         $participant = $this->getUser();
 
-        return $this->render('profil/profil.html.twig', [
-            'participant' => $participant,
-        ]);
+        return $this->render('profil/profil.html.twig', ['participant' => $participant] );
     }
 
+    #[Route('profil/modifier', name: 'profil_modifier')]
+    public function modifierProfile(ParticipantRepository $participantRepository): Response
+    {
+        $participant = $this->getUser();
 
+        $profil = new Participant();
+        $profilForm = $this->createForm(ProfilType::class, $profil);
+        return $this->render('profil/modifier.html.twig', ["ParticipantForm" => $profilForm->createView()]);
+    }
 
 
     #[Route('update', name: 'update')]
     public function modifierProfil(): Response
     {
-        return $this->render('participant/update.html.twig', [
+        return $this->render('', [
             'controller_name' => 'ParticipantController',
         ]);
     }
