8b21d780dc93c512f636eb384c13d25a1bc0a4f1
Max <chenmax02@gmail.com>
2022-10-04 12:34:28 +0200

    Mise en commun des fichiers

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -1,12 +1,13 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Participant;
-use App\Form\ProfilType;
+use App\Form\ProfilFormType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', name: 'profil_')]
@@ -23,11 +24,27 @@ class ProfilController extends AbstractController
     }
 
     #[Route('profil/modifier', name: 'modifier')]
-    public function modifierProfile(ParticipantRepository $participantRepository): Response
+    public function modifierProfile(ParticipantRepository $participantRepository,
+                                    Request $request,
+                                    EntityManagerInterface $entityManager,
+                                    UserPasswordHasherInterface $passwordHasher): Response
     {
-        $profil = new Participant();
-        $profilForm = $this->createForm(ProfilType::class, $profil);
+        $participant = $this->getUser();
+        $profilForm = $this->createForm(ProfilFormType::class, $participant);
+        $profilForm->handleRequest($request);
 
-        return $this->render('profil/modifier.html.twig', ["participantForm" => $profilForm->createView()]);
+        if($profilForm->isSubmitted() && $profilForm->isValid()){
+            $hashedPassword = $passwordHasher->hashPassword(
+                $participant,
+                $profilForm->get('motPasse')->getData()
+            );
+            $participant->setMotPasse($hashedPassword);
+
+            $entityManager->persist($participant);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('profil/modifier.html.twig',
+            ['profilForm' => $profilForm->createView()]);
     }
 }
