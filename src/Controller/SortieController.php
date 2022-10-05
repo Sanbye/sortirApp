0ac382e04b6691e66d5f640e8b7408c543eef7a4
Max <chenmax02@gmail.com>
2022-10-05 14:14:10 +0200

    Ajout de la fonctionnalité créer une sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\CreateSortieType;
-use App\Form\SortiesFormType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,25 +16,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class SortieController extends AbstractController
 {
     #[Route('/modifier', name: 'modifier')]
-    public function update(SortieRepository $sortieRepository): Response
+    public function update(): Response
     {
-        return $this->render('sorties/modifier.html.twig',);
     }
 
     #[Route('/creer', name: 'creer')]
-    public function create(SortieRepository $sortieRepository): Response
-    {
-        $sorties = new Sortie();
-        $sortiesForm = $this->createForm(CreateSortieType::class, $sorties);
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
 
-        return $this->render('sorties/creer.html.twig', [ 'sortiesForm' => $sortiesForm->createView()]);
+        $sorties = new Sortie();
+
+        $sortiesForm = $this->createForm(CreateSortieType::class, $sorties);
+        $sortiesForm->handleRequest($request);
+
+        if ($sortiesForm->isSubmitted() && $sortiesForm->isValid()) {
+
+            $entityManager->persist($sorties);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('sorties/creer.html.twig', [
+            'sortiesForm' => $sortiesForm->createView()
+        ]);
     }
 
     #[Route('/annuler', name: 'annuler')]
     public function cancel(SortieRepository $sortieRepository): Response
     {
-        $sortiesForm = $this->createForm(SortiesFormType::class);
-
-        return $this->render('sorties/annuler.html.twig', ['sortiesForm' => $sortiesForm->createView()]);
     }
 }
--- src/Form/CreateSortieType.php
+++ src/Form/CreateSortieType.php
@@ -2,21 +2,17 @@
 
 namespace App\Form;
 
-<<<<<<< HEAD
 use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-=======
-use App\Entity\Sortie;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
->>>>>>> 19aac85ee9af1889515d52dbd5a24783ead46d9b
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,25 +22,17 @@ class CreateSortieType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class)
-<<<<<<< HEAD
             ->add('dateHeureDebut', DateType::class, ['label' => 'Date et heure de la sortie : '])
             ->add('dateLimiteInscription', DateType::class, ['label' => 'Date limite d\'inscription : '])
             ->add('nbInscriptionsMax', NumberType::class, ['label' => 'Nombre de places : '])
-            ->add('duree', TimeType::class, ['label' => 'Durée'])
+            ->add('duree', NumberType::class, ['label' => 'Durée'])
             ->add('infosSortie', TextareaType::class, ['label' => 'Description et infos : '])
             ->add('campus', EntityType::class, ['class' => Campus::class, 'choice_label' => 'nom'])
-            ->add('lieu', TextType::class)
-=======
-            ->add('dateHeureDebut', DateType::class, ['label' => 'Date et heure de la sortie'])
-            ->add('dateLimiteInscription', DateType::class, ['label' => 'Date limite d\'inscription'])
-            ->add('duree', TimeType::class)
-            ->add('nbInscriptionsMax', NumberType::class)
-            ->add('infosSortie', TextareaType::class)
-            ->add('lieu', TextType::class)
-            ->add('etat', ChoiceType::class)
->>>>>>> 19aac85ee9af1889515d52dbd5a24783ead46d9b
-        ;
+            ->add('lieu', EntityType::class, ['class' => Lieu::class, 'choice_label' => 'nom'])
+            ->add('etat', EntityType::class, ['class' => Etat::class, 'choice_label' => 'id'])
+            ->add('organisateur', EntityType::class, ['class' => Participant::class, 'choice_label' => 'prenom']);
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
